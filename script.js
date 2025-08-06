7ab5ff1128ff688c72a833b72c73484362091e15
ricksonmendonca <ricksonsilvam@gmail.com>
2025-08-05 22:38:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-fetch("ranking.json")
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById("ranking");
-    container.innerHTML = "<h2>Ranking Geral</h2>";
-    let tabela = "<table><tr><th>Jogador</th><th>PTS</th></tr>";
-    data.forEach(j => {
-      tabela += `<tr><td>${j.nome}</td><td>${j.pts}</td></tr>`;
-    });
-    tabela += "</table>";
-    container.innerHTML += tabela;
-  });
+
+document.getElementById('app').innerText = "Todos os dados do ranking serão exibidos aqui.";
